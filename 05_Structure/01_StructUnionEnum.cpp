21e1956b5faed09497e338d618f785b9a7bbf3d4
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-01-20 22:07:56 +0530

    Add files via upload

---
+++ 05_Structure/01_StructUnionEnum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+typedef struct employee
+{
+    /* data */
+    int eId; //4
+    char favChar; //1
+    float salary; //4
+} ep;
+
+union money
+/*Unions are similar to structures but they provide better memory management then structures. 
+Unions use shared memory so only 1 variable can be used at a time.*/
+{
+    /* data */
+    int rice; //4
+    char car; //1
+    float pounds; //4
+};
+
+
+int main(){
+    enum Meal{ breakfast, lunch, dinner};
+    Meal m1 = lunch;
+    cout<<(m1==2);
+    // cout<<breakfast;
+    // cout<<lunch<<endl;
+    // cout<<dinner<<endl; 
+    // union money m1;
+    // m1.rice = 34;
+    // m1.car = 'c';
+    // cout<<m1.car<<endl;
+
+    // ep harry;
+    // struct employee shubham;
+    // struct employee rohanDas;
+    // harry.eId = 1;
+    // harry.favChar = 'c';
+    // harry.salary = 120000000;
+    // cout<<"The value is "<<harry.eId<<endl; 
+    // cout<<"The value is "<<harry.favChar<<endl; 
+    // cout<<"The value is "<<harry.salary<<endl; 
+    return 0;
+}
