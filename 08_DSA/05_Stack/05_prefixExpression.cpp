c02ac152e71a326492ebe7c39de17f7bd0e2d526
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-02-04 14:06:20 +0530

    Create 05_prefixExpression.cpp

---
+++ 08_DSA/05_Stack/05_prefixExpression.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stack>
+#include<cmath>
+#include<math.h>
+
+using namespace std;
+
+int prefixEvaluation(string s){
+    stack<int> st;
+
+    for (int i = s.length()-1; i>=0; i--)
+    {
+        if(s[i]>='0' && s[i]<='9'){     //check whether the input is operand or not
+            st.push(s[i]-'0');  //s[i] is char, to convert char to int -'0' is used 
+        }
+        else{
+            int op1=st.top();
+            st.pop();
+            int op2=st.top();
+            st.pop();
+
+            switch (s[i])       //check whether the input is operator or not
+            {
+            case '+':
+                st.push(op1+op2);
+                break;
+            case '-':
+                st.push(op1-op2);
+                break;
+            case '*':
+                st.push(op1*op2);
+                break;
+            case '/':
+                st.push(op1/op2);
+                break;
+            case '^':
+                st.push(pow(op1,op2));
+                break;
+            
+            }
+        }
+    }
+
+    return st.top();
+    
+}
+
+int main(){
+    cout<<prefixEvaluation("-+7*45+20");
+
+return 0;
+}
+
+//-+7*45+20
