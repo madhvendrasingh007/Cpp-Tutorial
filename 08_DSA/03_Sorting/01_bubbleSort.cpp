3800ce3c4b8e24766c04dec4ae3b5befd47e99a4
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-03-30 11:22:37 +0530

    Update 01_bubbleSort.cpp

--- 08_DSA/03_Sorting/01_bubbleSort.cpp
+++ 08_DSA/03_Sorting/01_bubbleSort.cpp
@@ -16,7 +16,7 @@ int main(){
         cin>>arr[i];
     }
 
-    int counter=1;
+    int counter=1;      // for iterations
     while(counter<n){
         for(int i=0; i<n-counter; i++){
             if(arr[i]>arr[i+1]){
