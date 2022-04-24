4bab1fde89b562adfa928e2f22006e4915e188ed
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-04-24 18:29:00 +0530

    Vectors

--- 09_STL/04_vectors.cpp
+++ 09_STL/04_vectors.cpp
@@ -80,3 +80,45 @@ Iterators
     ->  crend() – Returns a constant reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
 
 */
+
+
+
+/*
+Element access:
+
+    
+    ->  reference operator [g] – Returns a reference to the element at position ‘g’ in the vector
+    
+    ->  at(g) – Returns a reference to the element at position ‘g’ in the vector
+    
+    ->  front() – Returns a reference to the first element in the vector
+    
+    ->  back() – Returns a reference to the last element in the vector
+    
+    ->  data() – Returns a direct pointer to the memory array used internally by the vector to store its owned elements.
+*/
+
+
+
+/*MODIFIERS
+
+    
+    ->  assign() – It assigns new value to the vector elements by replacing old ones
+    
+    ->  push_back() – It push the elements into a vector from the back
+    
+    ->  pop_back() – It is used to pop or remove elements from a vector from the back.
+    
+    ->  insert() – It inserts new elements before the element at the specified position
+    
+    ->  erase() – It is used to remove elements from a container from the specified position or range.
+    
+    ->  swap() – It is used to swap the contents of one vector with another vector of same type. Sizes may differ.
+    
+    ->  clear() – It is used to remove all the elements of the vector container
+    
+    ->  emplace() – It extends the container by inserting new element at position
+    
+    ->  emplace_back() – It is used to insert a new element into the vector container, the new element is added to the end of the vector
+
+*/
