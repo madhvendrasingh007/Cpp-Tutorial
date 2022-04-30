70ab95a8970166a16c32a4a2494dec9ab8e09b0c
madhvendra_007 <66571451+madhvendrasingh007@users.noreply.github.com>
2022-04-30 23:05:09 +0530

    Stack using STL

---
+++ 09_STL/13_stack.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> st;
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+    return 0;
+}
