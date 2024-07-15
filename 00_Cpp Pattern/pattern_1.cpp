051778e3005879c349e5f4b45152dba7161ab606
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-07-15 19:02:13 +0000

    initial commit

---
+++ 00_Cpp Pattern/pattern_1.cpp
@@ -0,0 +1,26 @@
+/*
+    ****
+    ****
+    ****
+    ****
+ */
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int c;
+    cin >> c;
+    
+    for(int i=0; i<4; i++){
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<"*";
+        }
+
+        cout<<endl;
+        
+    }
+
+}
