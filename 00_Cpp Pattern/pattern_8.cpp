19c391518e0fb76c3e9fe7cde5312adc0be85fde
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-07-15 20:01:11 +0000

    initial commit

---
+++ 00_Cpp Pattern/pattern_8.cpp
@@ -0,0 +1,43 @@
+/*
+    *******
+     *****
+      ***
+       *
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int c){
+    for (int i = 0; i < c; i++)
+    {
+        // space
+        for (int j = 0; j < i; j++)
+        {
+            /* code */
+        }
+        
+        // star
+        for (int j = 0; j < ; j++)
+        {
+            /* code */
+        }
+        
+
+        // space
+        for (int j = 0; j < i; j++)
+        {
+            /* code */
+        }
+        
+        
+    }
+    
+}
+
+int main(){
+    int c;
+    cin>>c;
+
+    print(c);
+}
